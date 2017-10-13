b648a1a27b2b9f7ded0042a9cd61befe3bd828d0
houkah26 <houkah26@gmail.com>
2017-10-13 11:48:56 -0700

    fix port on app.listen

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require("express");
 var path = require("path");
 var app = express();
-var rootpath = path.normalize(__dirname + "/qGen-app");
+var rootpath = path.join(__dirname + "/public");
 var port = process.env.PORT || 3001;
 
 app.use(express.static(rootpath));
-app.listen(3001, function(err) {
+app.listen(port, function(err) {
   if (err) console.error(err);
   console.log("server running on port 3001");
 });
